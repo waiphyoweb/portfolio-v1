5110a0dd92ea56e9747d44ebfdad3cf22f0b1a60
WaiPhyoOo <waiphyo24262@gmail.com>
2023-06-04 20:32:05 +0630

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index" />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: [
+        { link: {text: "Home", url: "/"}, content: "" },
+        { link: {text: "Contact", url: "/contact"} },
+        { link: {text: "My Projects", url: "/projects"} },
+        { link: {text: "Joke", url: "/joke"} },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Courier New', Courier, monospace;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Wai Phyo</a>
+            <div>
+                <ul class="navbar-nav">
+                    <li 
+                        class="nav-item"
+                        v-for="(page, index) in pages"
+                        :key="index"
+                    >
+                        <a 
+                            class="nav-link" 
+                            :class="{active: activePage == index}" 
+                            aria-current="page" 
+                            :href="page.link.url"
+                            @click.prevent="navLinkClick(index)"
+                        >{{ page.link.text }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        props: [ 'pages', 'activePage', 'navLinkClick'],
+    }
+</script>
